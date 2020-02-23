c772f7158f169ae96e725c0049958dbe58df008f
Jiri Petrlik <jiripetrlik@gmail.com>
2020-02-23 21:23:53 +0100

    Basic CGP structure

---
+++ cgp/Cgp.go
@@ -0,0 +1,12 @@
+package cgp
+
+type Cgp struct {
+	values int[]
+	chromosomes int[][]
+	chromosomeMaxValues int[]
+	mutationProbability flofloat32
+}
+
+func initialize(size int, numberOfChromosomes int, mutationProbability floatfloat32) {
+	
+}
