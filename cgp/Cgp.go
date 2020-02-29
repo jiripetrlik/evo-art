682b9d2b97a520a0b60ee76350ea156083baaa9e
Jiri Petrlik <jiripetrlik@gmail.com>
2020-02-29 17:39:59 +0100

    First attempt to implement CGP

--- cgp/Cgp.go
+++ cgp/Cgp.go
@@ -1,12 +1,116 @@
 package cgp
 
+import "math/rand"
+
+const numberOfOperations = 15
+const maxParameterValue = 256
+
 type Cgp struct {
-	values int[]
-	chromosomes int[][]
-	chromosomeMaxValues int[]
-	mutationProbability flofloat32
+	chromosomes         [][]int
+	chromosomeMaxValues []int
+	outputs             int
+	mutationProbability float32
 }
 
-func initialize(size int, numberOfChromosomes int, mutationProbability floatfloat32) {
-	
+func NewCgp(inputs int, size int, outputs int, numberOfChromosomes int, mutationProbability float32) *Cgp {
+	var cgp Cgp
+
+	cgp.chromosomes = make([][]int, numberOfChromosomes)
+	for i := 0; i < numberOfChromosomes; i++ {
+		cgp.chromosomes[i] = make([]int, 4*size+outputs)
+	}
+
+	for i := 0; i < size; i++ {
+		cgp.chromosomeMaxValues[i*4] = inputs + i
+		cgp.chromosomeMaxValues[i*4+1] = inputs + i
+		cgp.chromosomeMaxValues[i*4+2] = numberOfOperations
+		cgp.chromosomeMaxValues[i*4+3] = maxParameterValue
+	}
+
+	outputsOffset := 4 * size
+	for i := 0; i < size; i++ {
+		cgp.chromosomeMaxValues[outputsOffset+i] = size
+	}
+
+	cgp.outputs = outputs
+	cgp.mutationProbability = mutationProbability
+
+	return &cgp
+}
+
+func (cgp Cgp) Randomize() {
+	numberOfChromosomes := len(cgp.chromosomes)
+	size := len(cgp.chromosomeMaxValues)
+	for i := 0; i < numberOfChromosomes; i++ {
+		for j := 0; j < size; j++ {
+			cgp.chromosomes[i][j] = rand.Intn(cgp.chromosomeMaxValues[j])
+		}
+	}
+}
+
+func (cgp Cgp) GetChromosome(number int) *[]int {
+	size := len(cgp.chromosomeMaxValues) - cgp.outputs
+	chromosome := make([]int, size)
+	for i := 0; i < size; i++ {
+		chromosome[i] = cgp.chromosomes[number][i]
+	}
+
+	return &chromosome
+}
+
+func (cgp Cgp) SetParent(parent []int) {
+	size := len(cgp.chromosomeMaxValues) - cgp.outputs
+	for i := 0; i < size; i++ {
+		cgp.chromosomes[0][i] = parent[i]
+	}
+}
+
+func (cgp Cgp) Mutate() {
+	numberOfChromosomes := len(cgp.chromosomes)
+	size := len(cgp.chromosomeMaxValues) - cgp.outputs
+	for i := 1; i < numberOfChromosomes; i++ {
+		for j := 0; j < size; j++ {
+			if rand.Float32() < cgp.mutationProbability {
+				cgp.chromosomes[i][j] = rand.Intn(cgp.chromosomeMaxValues[j])
+			} else {
+				cgp.chromosomes[i][j] = cgp.chromosomes[0][j]
+			}
+		}
+
+	}
+}
+
+func (cgp Cgp) evaluate(inputs []int, chromosomeNumber int) *[]int {
+	size := len(cgp.chromosomeMaxValues) - cgp.outputs
+	numberOfInputs := len(inputs)
+
+	values := make([]int, size)
+	for i := 0; i < size; i++ {
+		leftOperandAddress := cgp.chromosomes[chromosomeNumber][4*i]
+		rightOperandAddress := cgp.chromosomes[chromosomeNumber][4*i+1]
+		functionNumber := cgp.chromosomes[chromosomeNumber][4*i+2]
+		parameter := cgp.chromosomes[chromosomeNumber][4*i+3]
+
+		var leftOperand int
+		var rightOperand int
+		if leftOperandAddress < numberOfInputs {
+			leftOperand = inputs[leftOperandAddress]
+		} else {
+			leftOperand = values[leftOperandAddress-numberOfInputs]
+		}
+		if rightOperandAddress < numberOfInputs {
+			rightOperand = inputs[rightOperandAddress]
+		} else {
+			rightOperand = values[rightOperandAddress-numberOfInputs]
+		}
+
+		values[i] = evaluateFunction(leftOperand, rightOperand, functionNumber, parameter)
+	}
+
+	outputs := make([]int, cgp.outputs)
+	for i := 0; i < cgp.outputs; i++ {
+		outputs[i] = values[cgp.chromosomes[chromosomeNumber][4*size+i]]
+	}
+
+	return &outputs
 }
---
+++ cgp/CgpFunctions.go
@@ -0,0 +1,53 @@
+package cgp
+
+import "math"
+
+func evaluateFunction(leftOperand int, rightOperand int, functionNumber int, parameter int) int {
+	switch functionNumber {
+	case 0:
+		return leftOperand | rightOperand
+	case 1:
+		return leftOperand & rightOperand
+	case 2:
+		return leftOperand / (1 + rightOperand + parameter)
+	case 3:
+		return (leftOperand * rightOperand) % 255
+	case 4:
+		if leftOperand > rightOperand {
+			return leftOperand - rightOperand
+		} else {
+			return rightOperand - leftOperand
+		}
+	case 5:
+		return 255 - leftOperand
+	case 6:
+		return int(math.Abs(math.Cos(float64(leftOperand)) * 255))
+	case 7:
+		return int(math.Abs(math.Sin(float64(leftOperand)) * 255))
+	case 8:
+		return int(math.Abs(math.Tan((((float64(leftOperand % 45)) * math.Pi) / 180)) * 255))
+	case 9:
+		return int(math.Abs(float64(int(math.Tan(float64(leftOperand))*255) % 255)))
+	case 10:
+		value := math.Pow(float64(leftOperand)-float64(parameter), 2) + math.Pow(float64(rightOperand)-float64(parameter), 2)
+		if value <= 255 {
+			return int(value)
+		} else {
+			return 255
+		}
+	case 11:
+		return leftOperand%(parameter+1) + (255 - parameter)
+	case 12:
+		return (leftOperand + rightOperand) / 2
+	case 13:
+		if leftOperand > rightOperand {
+			return 255 * ((rightOperand + 1) / (leftOperand + 1))
+		} else {
+			return 255 * ((leftOperand + 1) / (rightOperand + 1))
+		}
+	case 14:
+		return int(math.Sqrt(math.Pow(float64(leftOperand), 2)+math.Pow(float64(rightOperand), 2)-2*math.Pow(float64(parameter), 2))) % 255
+	}
+
+	return 0
+}
