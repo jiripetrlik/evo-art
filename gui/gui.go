48022d36927dee49321919a66f02f956792010fa
Jiri Petrlik <jiripetrlik@gmail.com>
2020-03-04 21:26:33 +0100

    Display basic images

--- gui/gui.go
+++ gui/gui.go
@@ -2,6 +2,9 @@ package gui
 
 import (
 	"html/template"
+	"image"
+	"image/color"
+	"image/png"
 	"net/http"
 )
 
@@ -23,9 +26,27 @@ func cssEndpoint(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte(css))
 }
 
+func imageEndpoint(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "image/png")
+
+	const width = 256
+	const height = 256
+
+	r := image.Rect(0, 0, width, height)
+	img := image.NewRGBA(r)
+	c := color.RGBA{0, 0, 255, 255}
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.SetRGBA(x, y, c)
+		}
+	}
+	png.Encode(writer, img)
+}
+
 func GuiServer() {
 	http.HandleFunc("/", choiceEndpoint)
 	http.HandleFunc("/style.css", cssEndpoint)
+	http.HandleFunc("/image.png", imageEndpoint)
 
 	http.ListenAndServe(":8080", nil)
 }
--- gui/templates.go
+++ gui/templates.go
@@ -11,12 +11,12 @@ const choicePageTemplate string = `
 		<h1>EVO ART</h1>
 		<table class="choice_table">
 			<tr>
-				<td><img src="picture1.png"></td>
-				<td><img src="picture2.png"></td>
+				<td><img src="image.png"></td>
+				<td><img src="image.png"></td>
 			</tr>
 			<tr>
-				<td><img src="picture3.png"></td>
-				<td><img src="picture4.png"></td>
+				<td><img src="image.png"></td>
+				<td><img src="image.png"></td>
 			</tr>
 		</table>
 	</body>
